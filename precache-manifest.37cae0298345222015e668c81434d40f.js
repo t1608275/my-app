7cc18da344df0eb87ab53b94f27995cdb9848aeb
tamurkhan <t.khan7@wlv.ac.uk>
2019-04-26 22:41:40 +0100

    Updates

---
+++ precache-manifest.37cae0298345222015e668c81434d40f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "ec58c0ca38ce89052ad4",
+    "url": "/my-app/static/css/main.e6c85048.chunk.css"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "566a9ea4ff8a0a52205645e1a7ce409f",
+    "url": "/my-app/static/media/bg.566a9ea4.jpg"
+  },
+  {
+    "revision": "941ea86fac10582aa84f",
+    "url": "/my-app/static/js/2.8bf12b71.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "ec58c0ca38ce89052ad4",
+    "url": "/my-app/static/js/main.0e83b4a6.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "941ea86fac10582aa84f",
+    "url": "/my-app/static/css/2.e24e79c4.chunk.css"
+  },
+  {
+    "revision": "4ade095316079c26f25426f2afae7a31",
+    "url": "/my-app/index.html"
+  }
+];
