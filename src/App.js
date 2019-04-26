dbaad65636decc7581cc97b1ee703009e709c2b8
tamurkhan <t.khan7@wlv.ac.uk>
2019-04-26 22:33:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "0ecf6ee1a96564c76ab9180937e8a933";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    wind: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json();
+
+    if (city && country) {
+      // eslint-disable-next-line
+      if (data.cod == 404) {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          wind: undefined,
+          description: undefined,
+          error: "Location not found"
+        });
+      } else {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          wind: data.wind.speed,
+          description: data.weather[0].description,
+          error: ""
+        });
+      }
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        wind: undefined,
+        description: undefined,
+        error: "Fill in boxes!"
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    wind={this.state.wind}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => (
+  <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h4 className="title-container__subtitle">
+      Find out the temperature and much more...
+    </h4>
+  </div>
+);
+
+export default Title;
